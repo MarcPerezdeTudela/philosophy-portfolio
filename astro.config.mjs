73b0b45683292a685b79b3852b3d1863a24ec091
Marc Perez de Tudela <mperezdetudelaaznar@gmail.com>
2024-09-06 19:07:06 +0200

    add AstroIcons and change icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+import playformInline from "@playform/inline";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), playformInline(), icon()]
 });
